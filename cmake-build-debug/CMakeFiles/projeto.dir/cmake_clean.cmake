433981bef730b372739f4f8ceb7c61d04eb54588
André Flores <andrejesusferflores@gmail.com>
2020-10-13 12:01:01 +0100

    Started project

---
+++ cmake-build-debug/CMakeFiles/projeto.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projeto.dir/main.cpp.obj"
+  "libprojeto.dll.a"
+  "projeto.exe"
+  "projeto.exe.manifest"
+  "projeto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projeto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
