0ff239f06815c3a34a1bacbf5a9de00ffe4241e6
André Flores <andrejesusferflores@gmail.com>
2020-11-21 17:00:36 +0000

    Fixed << and >>  in Sala and Aderente

--- Sala.cpp
+++ Sala.cpp
@@ -47,22 +47,12 @@ bool Sala::operator<(const Sala &s1) const {
 }
 
 istream &operator>>(istream &input, Sala &s1) {
-    string s;
-    int pos = 0;
-    getline(input, s);
-    for (int i = 0; i < s.size(); i++){
-        if ((int(s[i]) >= 48) && (int(s[i]) <= 57)){
-            if (pos = 0) {pos = i;}
-            break;
-        }
-    }
-    s1.capacity = stoi(s.substr(pos, (s.size() - pos)));
-    s1.name = s.substr(0, pos);
+    getline(input, s1.name, '\t') >> s1.capacity;
     return input;
 }
 
 ostream &operator<<(ostream &output, const Sala &s1) {
-    output << s1.name << " " << s1.capacity;
+    output << s1.name << '\t' << s1.capacity;
     return output;
 }
 
